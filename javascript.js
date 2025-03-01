0e3e6adccc3d64b56db75cc571050221dae75b4a
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-03-01 12:45:05 +0330

    Implement getWinner function

--- javascript.js
+++ javascript.js
@@ -2,4 +2,33 @@ let answer = prompt("Please enter 'Rock', 'Paper', or 'Scissors'");
 console.log(`You chose ${answer}`);
 
 
+function getComputerChoice() {
+    
+}
 
+function getWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice)
+        return 'draw';
+
+    switch (playerChoice) {
+        case 'Rock':
+            if (computerChoice === 'Scissors')
+                return 'Player';
+            break;
+
+        case 'Paper':
+            if (computerChoice === 'Rock')
+                return 'Player';
+            break;
+
+        case 'Scissors':
+            if (computerChoice === 'Paper')
+                return 'Player';
+            break;
+
+        default:
+            break;
+    }
+
+    return 'Computer';
+}
