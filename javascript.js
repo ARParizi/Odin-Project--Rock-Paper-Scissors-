dc52dd4ae4b5fa0c269640251c5f69a3699f7020
ARParizi <52738836+ARParizi@users.noreply.github.com>
2025-03-01 13:06:31 +0330

    Implement getComputerChoice() & main loop

--- javascript.js
+++ javascript.js
@@ -1,9 +1,26 @@
-let answer = prompt("Please enter 'Rock', 'Paper', or 'Scissors'");
-console.log(`You chose ${answer}`);
-
+while (true) {
+    let answer = prompt("Please enter 'Rock', 'Paper', or 'Scissors'");
+    console.log(`You chose ${answer}`);
+    let computerChoice = getComputerChoice();
+    console.log(`Computer chose ${computerChoice}`);
+    console.log(`Winner is: ${getWinner(answer.toLowerCase(), computerChoice.toLowerCase())}`);   
+}
 
 function getComputerChoice() {
+    let randomInt = Math.floor(3 * Math.random());
+    switch (randomInt) {
+        case 0:
+            return 'Rock'
+
+        case 1:
+            return 'Paper'
+
+        case 2:
+            return 'Scissors'
     
+        default:
+            throw new Error("Impossible outcome");
+    }
 }
 
 function getWinner(playerChoice, computerChoice) {
@@ -11,18 +28,18 @@ function getWinner(playerChoice, computerChoice) {
         return 'draw';
 
     switch (playerChoice) {
-        case 'Rock':
-            if (computerChoice === 'Scissors')
+        case 'rock':
+            if (computerChoice === 'scissors')
                 return 'Player';
             break;
 
-        case 'Paper':
-            if (computerChoice === 'Rock')
+        case 'paper':
+            if (computerChoice === 'rock')
                 return 'Player';
             break;
 
-        case 'Scissors':
-            if (computerChoice === 'Paper')
+        case 'scissors':
+            if (computerChoice === 'paper')
                 return 'Player';
             break;
 
